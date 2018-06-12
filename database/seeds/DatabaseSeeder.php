f60a1746ed7ae8c11b8123a0de8da99022655c3f
aihtshamali <14024020154@umt.edu.pk>
2018-06-12 15:13:49 +0500

    Completed

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+      factory(App\User::class, 25)->create()->each(function ($u) {
+        for ($i=0; $i <= 3; $i++) {
+          $u->posts()->save(factory(App\Post::class)->make());
+        }
+      });
+        // $this->call(UsersTableSeeder::class);
+    }
+}
